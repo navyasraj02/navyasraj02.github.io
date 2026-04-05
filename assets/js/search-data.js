c114d0c9031da09d3207d8037dccc2d2c6bc7564
navyasraj02 <84143583+navyasraj02@users.noreply.github.com>
2026-04-05 21:50:21 +0000

    Deploying to gh-pages from @ navyasraj02/navyasraj02.github.io@0298a2ffbcd594b79cd6efb467aff339ed7c7ba2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,9 +461,9 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-pseudostem-weevil-detector-pswd",
+          title: 'Pseudostem Weevil Detector (PSWD)',
+          description: "Acoustic pest detection in banana crops using CNNs.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
