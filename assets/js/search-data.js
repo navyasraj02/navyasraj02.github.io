c30f6270b2e4ef415a88a864985cdcae4302f836
navyasraj02 <84143583+navyasraj02@users.noreply.github.com>
2026-04-09 01:11:55 +0000

    Deploying to gh-pages from @ navyasraj02/navyasraj02.github.io@2d45d35975b8c688deab7a27aacc3479864e5e6b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -425,6 +425,11 @@ ninja.data = [{
           description: "A decentralized biometric system using template fragmentation and encrypted distribution.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
+            },},{id: "projects-my-simple-tezfunds",
+          title: 'My Simple TezFunds',
+          description: "A blockchain-based crowdfunding platform for women-led businesses, built for the TezAsia’22 Hackathon.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
