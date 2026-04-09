5a4a2b7b61746ae3867b0833b0be9cce2d7e63f2
navyasraj02 <84143583+navyasraj02@users.noreply.github.com>
2026-04-09 01:18:02 +0000

    Deploying to gh-pages from @ navyasraj02/navyasraj02.github.io@18ffdaa3e78e5f805edb7943d887ed76283f4842 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-certifications",
+          title: "Certifications",
+          description: "Certifications in AI, Machine Learning, and Full-Stack Web Development.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/certifications/";
+          },
+        },{id: "nav-projects",
           title: "Projects",
           description: "",
           section: "Navigation",
